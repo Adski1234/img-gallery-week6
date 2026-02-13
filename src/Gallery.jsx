e9fc6e9a5dc5c508d96377f0ac154a729017d274
ADAM RICHARDSON <ADAM.RICHARDSON123@GMAIL.COM>
2026-02-13 14:27:19 +0000

    Buttons and thumbnail

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -3,8 +3,9 @@ import Image from './Image';
 
 const Gallery = () => {
     const [images, setImages] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-    useEffect(() => {
+     useEffect(() => {
         fetch('https://week-6-api.vercel.app/api/images')
             .then((response) => response.json())
             .then((data) => {
@@ -17,15 +18,49 @@ const Gallery = () => {
                 setLoading(false);      
             });
             }, []);
-        
+
+            const nextImage = () => {
+                setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+            };
+
+                const prevImage = () => {
+                setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+                };
+
             return (
-                <div className="gallery">
-                    {images.length > 0 && 
-                    images.map((image) => (
-                        <Image key={image.id} title={image.title} src={image.url}  alt={image.alt}  />
-                    ))} 
+                <div className="gallery-container">
+                    <div className="main-image-container">
+                        {images.length > 0 && (
+               <img
+                src={images[currentIndex].url}
+                alt={images[currentIndex].alt}  />
+                )}
                 </div>
+
+
+                    <div className="thumbnail-bar">
+                         {images.map((image, index) => (
+                                <img
+                                key={image.id}
+                                className={`thumbnail ${index === currentIndex ? 'active' : ''}`}
+                                src={image.url}
+                                alt={image.alt}
+                                onClick={() => setCurrentIndex(index)}
+                                />
+                            ))}
+                        </div>
+
+                        <div className="arrows">
+                            <button onClick={prevImage} className="arrow left-arrow">
+                                {"<"}
+                            </button>
+                            <button onClick={nextImage} className="arrow right-arrow">
+                                {">"}
+                            </button>
+                            </div>
+                        </div>
             );
         };
         
+                     
 export default Gallery;
